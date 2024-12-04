3ac1e9bfe58c7fbe893414ea05126a6fb14f0ca1
Vince <vhorvath2010@gmail.com>
2024-12-03 19:48:14 -0500

    day 1 - part 1

--- day-01/main.ts
+++ day-01/main.ts
@@ -4,8 +4,22 @@ async function loadPairs(filePath: string): Promise<number[][]> {
     .split("\r\n")
     .map((stringPair) => stringPair.split("   "))
     .map((pair) => pair.map((num) => parseInt(num)));
-  return numberPairs;
+  return numberPairs[0].map((_, colIndex) => numberPairs.map((row) => row[colIndex]));
 }
 
 const pairs = await loadPairs("input.txt");
-console.log(`Loaded ${pairs.length} pairs. The first is ${pairs[0]}`);
+const firstList = pairs[0];
+const secondList = pairs[1];
+const n = firstList.length;
+console.log(`Loaded ${n} pairs`);
+
+firstList.sort();
+secondList.sort();
+console.log("Lists sorted");
+
+console.log("Calculating diff...");
+let sum = 0;
+for (let i = 0; i < n; i++) {
+  sum += Math.abs(firstList[i] - secondList[i]);
+}
+console.log(sum);
