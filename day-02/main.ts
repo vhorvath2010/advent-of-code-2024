df64adbeeb52c5a38d4fc3597c712e8610700e6f
Vince <vhorvath2010@gmail.com>
2024-12-04 23:41:13 -0500

    first attempt at day 2 (needs work)

---
+++ day-02/main.ts
@@ -0,0 +1,31 @@
+async function loadReports(filePath: string): Promise<number[][]> {
+  const fileText = await Deno.readTextFile(filePath);
+  const reports = fileText
+    .split("\r\n")
+    .map((stringPair) => stringPair.split(" "))
+    .map((pair) => pair.map((num) => parseInt(num)));
+  return reports;
+}
+
+function isReportSafe(report: number[]): boolean {
+  const isIncreasing = report[1] > report[0];
+  for (let i = 0; i < report.length - 1; i++) {
+    const diff = report[i + 1] - report[i];
+    if (diff === 0 || Math.abs(diff) > 3) {
+      return false;
+    }
+    if (diff < 0 && isIncreasing) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const reports = await loadReports("input.txt");
+console.log(`Loaded ${reports.length} reports`);
+
+let safeReports = 0;
+reports.forEach((report) => {
+  safeReports += isReportSafe(report) ? 1 : 0;
+});
+console.log(`There are ${safeReports} safe reports`);
