c0dbbe2b892d59062276e9c3166e76e6e3169c96
Vince <vhorvath2010@gmail.com>
2024-12-05 20:21:58 -0500

    finish day 2 part 2

--- day-02/main.ts
+++ day-02/main.ts
@@ -7,15 +7,19 @@ async function loadReports(filePath: string): Promise<number[][]> {
   return reports;
 }
 
-function isReportSafe(report: number[]): boolean {
+function isReportSafe(report: number[], canTolerateBadLevel: boolean = false): boolean {
   const isIncreasing = report[1] > report[0];
   for (let i = 0; i < report.length - 1; i++) {
     const diff = report[i + 1] - report[i];
-    if (diff === 0 || Math.abs(diff) > 3) {
-      return false;
-    }
     const isDiffIncreasing = diff > 0;
-    if (isDiffIncreasing !== isIncreasing) {
+    if (diff === 0 || Math.abs(diff) > 3 || isDiffIncreasing !== isIncreasing) {
+      if (canTolerateBadLevel) {
+        return (
+          isReportSafe(report.toSpliced(i - 1, 1)) ||
+          isReportSafe(report.toSpliced(i, 1)) ||
+          isReportSafe(report.toSpliced(i + 1, 1))
+        );
+      }
       return false;
     }
   }
@@ -27,6 +31,6 @@ console.log(`Loaded ${reports.length} reports`);
 
 let safeReports = 0;
 reports.forEach((report) => {
-  safeReports += isReportSafe(report) ? 1 : 0;
+  safeReports += isReportSafe(report, true) ? 1 : 0;
 });
 console.log(`There are ${safeReports} safe reports`);
